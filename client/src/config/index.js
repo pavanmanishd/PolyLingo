1188c561cf0ecbde651f6067bcccb472661f381b
pavanmanishd <pavanmanishd@gmail.com>
2024-02-10 10:21:05 +0530

    Update API and SOCKET URLs

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,7 +1,7 @@
 const constants = {
-    API_URL: 'http://192.168.0.101:3000',
-    SOCKET_URL: 'http://192.168.0.101:3000',
-    TRANSLATE_API_URL: 'http://192.168.0.101:5000',
+    API_URL: 'http://10.64.57.60:3000',
+    SOCKET_URL: 'http://10.64.57.60:3000',
+    TRANSLATE_API_URL: 'http://10.64.57.60:5000',
 };
 
 
